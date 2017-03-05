95133a2ec8b68dd8397f00d042682cc4d80cf387
tmstani23 <tmstani23@gmail.com>
2017-03-05 17:25:58 -0600

    Player and mob damage update
    Added player and mob damage, healthbars and mob to player hit
    functionality.  Added kickback when player is hit by a mob.  Fixed
    player to wall collisions when pushed into a wall by a mob.

--- main.py
+++ main.py
@@ -9,6 +9,25 @@ from settings import *
 #import all from sprites.py file
 from sprites import *
 
+
+#HUD functions
+def draw_player_health(surf, x, y, pct):
+    if pct < 0:
+        pct = 0
+    BAR_LENGTH = 100
+    BAR_HEIGHT = 20
+    fill = pct * BAR_LENGTH
+    outline_rect = pg.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
+    fill_rect = pg.Rect(x, y, fill, BAR_HEIGHT)
+    if pct > 0.6:
+        col = GREEN
+    elif pct > 0.3:
+        col = YELLOW
+    else:
+        col = RED
+    pg.draw.rect(surf, col, fill_rect)
+    pg.draw.rect(surf, WHITE, outline_rect, 2)
+
 class Game:
     def __init__(self):
         #Initialize pygame and create window
@@ -36,7 +55,7 @@ class Game:
         self.bullet_img = pg.image.load(path.join(imgFolder, BULLET_IMG)).convert_alpha()
         self.mob_img = pg.image.load(path.join(imgFolder, MOB_IMG)).convert_alpha()
         self.wall_img = pg.image.load(path.join(imgFolder, WALL_IMG)).convert_alpha()
-        #self.wall_img = pg.transform.scale(self.wall_img, (tileSize, tileSize))
+        self.wall_img = pg.transform.scale(self.wall_img, (tileSize, tileSize))
 
     
     def newGame(self):
@@ -84,11 +103,26 @@ class Game:
         #Update game screen
         self.allSprites.update()
         self.camera.update(self.player)
+        #Mob hits player:
+        hits = pg.sprite.spritecollide(self.player, self.mobs, False, collide_hit_rect)
+        for hit in hits:
+            #subtract mob damage from player health
+            self.player.health -= MOB_DAMAGE
+            #move mob back a bit
+            hit.vel = vec(0, 0)
+            #game over if player health reaches 0
+            if self.player.health <= 0:
+                self.playing = False
+            if hits:
+                #player is knocked back a bit and continues facing same direction
+                self.player.pos += vec(MOB_KNOCKBACK, 0).rotate(-hits[0].rot)
+
         #Bullets hit mobs
         #false/true - mobs dont disappear bullets do when hit
         hits = pg.sprite.groupcollide(self.mobs, self.bullets, False, True)
         for hit in hits:
-            hit.kill()
+            hit.health -= BULLET_DAMAGE
+            hit.vel = vec(0, 0)
     
     def events(self):
         #Process input (events)
@@ -119,9 +153,14 @@ class Game:
         #self.drawGrid()
         #Draw sprites
         for sprite in self.allSprites:
+            #if sprite is a mob:
+            if isinstance(sprite, Mob):
+                sprite.draw_health()        
             self.screen.blit(sprite.image, self.camera.apply(sprite))
         #After drawing always flip the display
         #pg.draw.rect(self.screen, WHITE, self.player.hit_rect, 2)
+        #Draw HUD functions:
+        draw_player_health(self.screen, 10, 10, self.player.health / PLAYER_HEALTH)
         pg.display.flip()
     
     def showStartScreen(self):
